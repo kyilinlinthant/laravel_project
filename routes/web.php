761fd3a9b9b567d0b307c2b93c0fba77e45b43fb
kyilinlinthant <moepyarpyar28@gmail.com>
2020-07-29 14:04:45 +0630

    feature_week4

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// 
+Route::resource('receipe', 'ReceipeController');
+// Route::get('receipe', 'ReceipeController@index');
+// Route::get('receipe/create', 'ReceipeController@createReceipeForm');
+// Route::get('receipe/{id}', 'ReceipeController@show');
+// Route::post('receipe', 'ReceipeController@create');
+// Route::get('receipe/{id}/edit', 'ReceipeController@edit');
+// Route::patch('receipe/{id}', 'ReceipeController@update');
+// Route::delete('receipe/{id}', 'ReceipeController@delete');
+
